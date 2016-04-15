719538c07594117651934238b8e3cc24c582cf24
Christoph Pickl <c.pickl@meo-energy.com>
2016-04-15 21:05:54 +0200

    release script done (i hope)

--- release.sh
+++ release.sh
@@ -1,97 +1,130 @@
 #!/bin/bash
 
+START=`date +%s`
+
+CWD=`pwd`
+
+BUILD_DIR=$CWD/../gadsu_release_build
+ARTIFACTS_DIR=$BUILD_DIR/release_artifacts
+# unfortunately the fatJar plugin uses the folder name as the jar filename :-/
+CHECKOUT_DIR=$BUILD_DIR/Gadsu
+GIT_URL=https://github.com/christophpickl/gadsu.git
+
+myEcho() {
+    echo "[RELEASE] $1"
+}
+
 checkLastCommand() {
     if [ $? -ne 0 ] ; then
-        echo "Last command did not end successful!"
+        myEcho "Last command did not end successful!"
         exit 1
     fi
 }
 
 changeVersion() {
-    echo ""
-    echo "Changing version in [version.properties] to: $1"
+    echo
+    myEcho "Changing version in [version.properties] to: $1"
     echo "version=$1" > version.properties
-
+    checkLastCommand
 }
 
 source version.properties
 
-echo "Preparing new GADSU release"
-echo "======================="
-echo "Current version is: $version"
+myEcho "Preparing new GADSU release"
+myEcho "=================================="
+myEcho "Current version is: $version"
 
 echo
 read -p "Enter RELEASE Version: " VERSION_RELEASE
-read -p "Enter DEVELOPMENT Version: " VERSION_DEVELOPMENT
+read -p "Enter next DEVELOPMENT Version: " VERSION_DEVELOPMENT
 # maybe also prompt for git credentials?!?
 
-echo ""
-echo "Release Summary:"
-echo "-----------------------"
-echo "  Release Version: $VERSION_RELEASE"
-echo "  Development Version: $VERSION_DEVELOPMENT"
-echo ""
+echo
+myEcho "Release Summary:"
+myEcho "----------------------------------"
+myEcho "  Release Version: $VERSION_RELEASE"
+myEcho "  Development Version: $VERSION_DEVELOPMENT"
+echo
 
 
 while true; do
-    read -p "Do you confirm this release? [y/n] " yn
+    read -p "[RELEASE] Do you confirm this release? [y/n] >> " yn
     case $yn in
         [Yy]* ) break;;
         [Nn]* ) echo "Aborted"; exit;;
-        * ) echo "Please answer y(es) or n(o)";;
+        * ) myEcho "Please answer y(es) or n(o)";;
     esac
 done
+echo
 
-cd ..
-
-BUILD_DIR=gadsu_release_build
-ARTIFACTS_DIR=release_artifacts
-
+if [ -d "$BUILD_DIR" ]; then
+    echo
+    myEcho "Removing old build directory at: $BUILD_DIR"
+    rm -rf $BUILD_DIR
+fi
 mkdir $BUILD_DIR
-cd $BUILD_DIR
 mkdir $ARTIFACTS_DIR
-CWD=`pwd`
 
-ABSOLUTE_ARTIFACTS_DIR="$CWD/$ARTIFACTS_DIR"
+cd $BUILD_DIR
 
-echo ""
-echo "Checking out source to: $CWD/gadsu"
-git clone https://github.com/christophpickl/gadsu.git gadsu
-cd gadsu
+
+
+echo
+myEcho "Checking out source to: $CHECKOUT_DIR"
+myEcho "----------------------------------"
+git clone $GIT_URL $CHECKOUT_DIR
+cd $CHECKOUT_DIR
 
 # TODO check there are no snapshots
 
-echo ""
-echo "Running test build."
+echo
+myEcho "Running test build."
+myEcho "----------------------------------"
 ./gradlew test testUi check
 checkLastCommand
 
 changeVersion $VERSION_RELEASE
 
-echo ""
-echo "Creating assemblies."
+echo
+myEcho "Creating assemblies."
+myEcho "----------------------------------"
 ./gradlew createDmg fatJar -Dgadsu.enableMacBundle=true
 checkLastCommand
 
-cp build/distributions/*.dmg $ABSOLUTE_ARTIFACTS_DIR
-cp build/libs/*.jar $ABSOLUTE_ARTIFACTS_DIR
-echo "Copied artifacts to: $ABSOLUTE_ARTIFACTS_DIR"
+cp build/distributions/*.dmg $ARTIFACTS_DIR
+cp build/libs/*.jar $ARTIFACTS_DIR
+echo
+myEcho "Copied artifacts to: $ARTIFACTS_DIR"
 
-echo ""
-echo "GIT committing and tagging result"
+echo
+myEcho "GIT committing and tagging result"
+myEcho "----------------------------------"
+git add .
 git commit -m "[Auto-Release] current release version: $VERSION_RELEASE"
+checkLastCommand
 git tag $VERSION_RELEASE
+checkLastCommand
 
 changeVersion $VERSION_DEVELOPMENT
 
+git add .
 git commit -m "[Auto-Release] next development version: $VERSION_DEVELOPMENT"
+checkLastCommand
 
-# git push
-echo "TODO TODO TODO GIT push"
+git push
+checkLastCommand
 
-echo ""
-echo ""
-echo "Release done"
-# TODO keep track of time needed
+END=`date +%s`
+ELAPSED=$(( $END - $START ))
+
+echo
+echo
+myEcho "Release $VERSION_RELEASE done."
+myEcho "=================================="
+echo
+myEcho "Time needed: $ELAPSED seconds"
+myEcho "Copy the contents of the artifacts directory: $ARTIFACTS_DIR"
+ls -l $ARTIFACTS_DIR
+echo
 
 exit 0
