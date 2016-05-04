f6bcbea21a6d1d270e3ef37b77df670f8faf2614
Christoph Pickl <christoph.pickl@gmail.com>
2016-05-04 15:57:57 +0200

    misc

--- src/main/kotlin/at/cpickl/gadsu/report/playground.kt
+++ src/main/kotlin/at/cpickl/gadsu/report/playground.kt
@@ -14,6 +14,7 @@ import java.io.FileOutputStream
 
 
 fun main(args: Array<String>) {
+
 //    createPdf("text.pdf")
 //    manipulatePdf("cover.pdf", "cover2.pdf")
     addCover("text.pdf", "text_with_cover.pdf")
