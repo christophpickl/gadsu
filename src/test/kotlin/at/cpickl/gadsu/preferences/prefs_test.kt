0bf5c6b534d47da116dc31c3a83bf4ebce97b6b9
Christoph Pickl <christoph.pickl@gmail.com>
2016-07-25 11:29:35 +0200

    fixing the test

--- src/test/kotlin/at/cpickl/gadsu/preferences/prefs_test.kt
+++ src/test/kotlin/at/cpickl/gadsu/preferences/prefs_test.kt
@@ -13,7 +13,7 @@ import java.util.prefs.Preferences
 
     private val testDescriptor = WindowDescriptor(Point(1, 2), Dimension(300, 400))
     private val testData = PreferencesData("testUsername", true, "proxy:42", "gcal_calendar_name", 20)
-    private val testPicFolder = File("testPicFolder/foobar/")
+    private val testPicFolder = File("src/test/resources")
 
     private val prefNode = javaClass
     private var prefs = JavaPrefs(prefNode)
