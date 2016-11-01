3607f683d6f0e87ba4b410bdf4129c01a3c34b7f
Christoph Pickl <christoph.pickl@gmail.com>
2016-11-01 10:47:16 +0100

    #17 fixing the test

--- src/test/kotlin/at/cpickl/gadsu/treatment/dyn_model_test.kt
+++ src/test/kotlin/at/cpickl/gadsu/treatment/dyn_model_test.kt
@@ -8,13 +8,21 @@ import org.testng.annotations.Test
 class DynTreatmentFactoryTest {
 
     fun `managersForAllExcept given empty except should return all dyn treatments managers`() {
-        assertThat(DynTreatmentFactory.managersForAllExcept(emptyList()),
-                equalTo(listOf(HaraDiagnosisManager, BloodPressureManager)))
+        assertThat(
+                DynTreatmentFactory.managersForAllExcept(emptyList()),
+                equalTo(DynTreatmentFactory.all))
+    }
+
+    fun `managersForAllExcept given all except should return empty managers`() {
+        assertThat(
+                DynTreatmentFactory.managersForAllExcept(DynTreatmentFactory.all.map { it.create() }),
+                empty())
     }
 
     fun `managersForAllExcept given hara as except should return all except hara manager`() {
         val except = listOf<DynTreatment>(HaraDiagnosis.insertPrototype())
-        assertThat(DynTreatmentFactory.managersForAllExcept(except),
+        assertThat(
+                DynTreatmentFactory.managersForAllExcept(except),
                 allOf(
                         not(contains<DynTreatmentManager>(HaraDiagnosisManager)),
                         hasSize(DynTreatmentFactory.all.size - except.size)
