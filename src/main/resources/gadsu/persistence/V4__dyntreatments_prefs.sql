c07ecb68db4561d224d8f2c73fdab0c9d9f3ee04
Christoph Pickl <christoph.pickl@gmail.com>
2016-11-08 00:22:16 +0100

    #17 moving prefs SQL into common V4 migration script

--- src/main/resources/gadsu/persistence/V4__dyntreatments_prefs.sql
+++ src/main/resources/gadsu/persistence/V4__dyntreatments_prefs.sql
@@ -10,3 +10,12 @@ CREATE TABLE hara_diagnosis (
 
   FOREIGN KEY (id_treatment) REFERENCES treatment(id)
 );
+
+
+-- #59 rewrite preferences to use database instead of java prefs api
+-- ========================================================================= --
+
+CREATE TABLE prefs (
+  data_key VARCHAR(128) NOT NULL PRIMARY KEY,
+  data_value VARCHAR(1024) NOT NULL
+);
