8a25a71deb4d58b60e20af799f7c808af3462492
Christoph Pickl <christoph.pickl@gmail.com>
2016-11-15 13:37:13 +0100

    #17 more tongue requirements

--- src/main/kotlin/at/cpickl/gadsu/treatment/dyn/treats/tongue.kt
+++ src/main/kotlin/at/cpickl/gadsu/treatment/dyn/treats/tongue.kt
@@ -21,7 +21,7 @@ private val TITLE_TONGUE = "Zungendiagnose"
 
 data class TongueDiagnosis(
         // FIXME #17 enum opts (display some nice image in UI)
-        val note: String
+        val note: String // um zu speichern wo zb die schwellung ist, etc.
 ) : DynTreatment {
     companion object {
         fun insertPrototype() = TongueDiagnosis("")
