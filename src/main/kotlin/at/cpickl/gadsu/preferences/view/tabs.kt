cb184e529a5df3066b00f9ab03c86883e378ec70
Christoph Pickl <c.pickl@meo-energy.com>
2017-03-30 15:00:06 +0200

    fixing compile/merge errors

--- src/main/kotlin/at/cpickl/gadsu/preferences/view/tabs.kt
+++ src/main/kotlin/at/cpickl/gadsu/preferences/view/tabs.kt
@@ -2,6 +2,7 @@ package at.cpickl.gadsu.preferences.view
 
 import at.cpickl.gadsu.client.xprops.view.GridBagFill
 import at.cpickl.gadsu.version.CheckForUpdatesEvent
+import at.cpickl.gadsu.view.KTab
 import at.cpickl.gadsu.view.SwingFactory
 import at.cpickl.gadsu.view.components.MyTextArea
 import at.cpickl.gadsu.view.components.inputs.HtmlEditorPane
@@ -19,16 +20,14 @@ import java.awt.GridBagConstraints
 import javax.swing.BorderFactory
 import javax.swing.BoxLayout
 import javax.swing.JCheckBox
-import javax.swing.JComponent
 import javax.swing.JPanel
 import javax.swing.JTextField
 
 
-abstract class PrefsTab(val tabTitle: String) {
+abstract class PrefsTab(override val tabTitle: String, override val scrolled: Boolean = true) : KTab {
 
     protected val VGAP_BETWEEN_COMPONENTS = 10
 
-    abstract fun asComponent(): JComponent
 }
 
 class PrefsTabGeneral(swing: SwingFactory) : PrefsTab("Allgemein") {
@@ -42,7 +41,7 @@ class PrefsTabGeneral(swing: SwingFactory) : PrefsTab("Allgemein") {
 
     val btnCheckUpdate = swing.newEventButton("Jetzt prüfen", "", { CheckForUpdatesEvent() })
 
-    override fun asComponent() = FormPanel(labelAnchor = GridBagConstraints.NORTHWEST).apply {
+    override fun asComponent() = FormPanel(fillCellsGridy = false, labelAnchor = GridBagConstraints.NORTHWEST).apply {
         border = BorderFactory.createEmptyBorder(10, HGAP_FROM_WINDOW, 0, HGAP_FROM_WINDOW)
 
         addDescriptiveFormInput("Dein Name", inpUsername, "Dein vollständiger Name wird unter anderem<br/>auf Rechnungen und Berichte (Protokolle) angezeigt.")
@@ -81,6 +80,7 @@ class PrefsTabConnectivity : PrefsTab("Connectivity") {
     val inpConfirmMailBody = MyTextArea("", visibleRows = 6)
 
     override fun asComponent() = FormPanel(
+            fillCellsGridy = false,
             labelAnchor = GridBagConstraints.NORTHWEST,
             inputAnchor = GridBagConstraints.NORTHWEST).apply {
         border = BorderFactory.createEmptyBorder(10, HGAP_FROM_WINDOW, 0, HGAP_FROM_WINDOW)
--- src/main/kotlin/at/cpickl/gadsu/preferences/view/window.kt
+++ src/main/kotlin/at/cpickl/gadsu/preferences/view/window.kt
@@ -1,6 +1,8 @@
-package at.cpickl.gadsu.preferences
+package at.cpickl.gadsu.preferences.view
 
 import at.cpickl.gadsu.client.xprops.view.GridBagFill
+import at.cpickl.gadsu.preferences.PreferencesData
+import at.cpickl.gadsu.preferences.PreferencesWindowClosedEvent
 import at.cpickl.gadsu.service.GapiCredentials
 import at.cpickl.gadsu.service.nullIfEmpty
 import at.cpickl.gadsu.version.CheckForUpdatesEvent
@@ -32,82 +34,6 @@ interface PreferencesWindow : ClosableWindow, WritablePreferencesWindow {
     fun start()
     fun initData(preferencesData: PreferencesData)
     fun readData(): PreferencesData
-}
-
-private abstract class PrefsTab(override val tabTitle: String) : KTab {
-    protected val VGAP_BETWEEN_COMPONENTS = 10
-    override val scrolled = true
-}
-
-private class PrefsTabGeneral(swing: SwingFactory) : PrefsTab("Allgemein") {
-
-    val inpUsername = JTextField().viewName { Preferences.InputUsername }
-    val inpCheckUpdates = JCheckBox("Beim Start prüfen")
-    val inpTreatmentGoal = NumberField(4).selectAllOnFocus().leftAligned()
-
-    val inpApplicationDirectory = JTextField().disabled().disableFocusable()
-    val inpLatestBackup = JTextField().disabled().disableFocusable()
-
-    val btnCheckUpdate = swing.newEventButton("Jetzt prüfen", "", { CheckForUpdatesEvent() })
-
-    override fun asComponent() = FormPanel(fillCellsGridy = false, labelAnchor = GridBagConstraints.NORTHWEST).apply {
-        border = BorderFactory.createEmptyBorder(10, HGAP_FROM_WINDOW, 0, HGAP_FROM_WINDOW)
-
-        addDescriptiveFormInput("Dein Name", inpUsername, "Dein vollständiger Name wird unter anderem<br/>auf Rechnungen und Berichte (Protokolle) angezeigt.")
-        addDescriptiveFormInput("Auto Update", initPanelCheckUpdates(), "Um immer am aktuellsten Stand zu bleiben,<br/>empfiehlt es sich diese Option zu aktivieren.",
-                GridBagFill.None, addTopInset = VGAP_BETWEEN_COMPONENTS)
-        addDescriptiveFormInput("Behandlungsziel*", inpTreatmentGoal, "Setze dir ein Ziel wieviele (unprotokollierte) Behandlungen du schaffen m\u00f6chtest.")
-
-        addDescriptiveFormInput("Programm Ordner", inpApplicationDirectory, "Hier werden die progamm-internen Daten gespeichert.",
-                addTopInset = VGAP_BETWEEN_COMPONENTS)
-        addDescriptiveFormInput("Letztes Backup", inpLatestBackup, "Gadsu erstellt für dich täglich ein Backup aller Informationen.",
-                addTopInset = VGAP_BETWEEN_COMPONENTS)
-
-        c.gridwidth = 2
-        add(HtmlEditorPane("<b>*</b> ... <i>Neustart erforderlich</i>").disableFocusable())
-        addLastColumnsFilled()
-    }
-
-    private fun initPanelCheckUpdates() = JPanel().apply {
-        layout = BoxLayout(this, BoxLayout.X_AXIS)
-        transparent()
-
-        add(inpCheckUpdates)
-        add(btnCheckUpdate)
-    }
-
-}
-
-private class PrefsTabConnectivity : PrefsTab("Connectivity") {
-
-    val inpProxy = JTextField()
-    val inpGcalName = JTextField()
-    val inpGmailAddress = JTextField()
-    val inpGapiClientId = JTextField()
-    val inpGapiClientSecret = JTextField()
-    val inpConfirmMailSubject = JTextField()
-    val inpConfirmMailBody = MyTextArea("", visibleRows = 6)
-
-    override fun asComponent() = FormPanel(
-            fillCellsGridy = false,
-            labelAnchor = GridBagConstraints.NORTHWEST,
-            inputAnchor = GridBagConstraints.NORTHWEST).apply {
-        border = BorderFactory.createEmptyBorder(10, HGAP_FROM_WINDOW, 0, HGAP_FROM_WINDOW)
-
-        addDescriptiveFormInput("HTTP Proxy*", inpProxy, "Falls du \u00fcber einen Proxy ins Internet gelangst,<br/>dann konfiguriere diesen bitte hier. (z.B.: <tt>proxy.heim.at:8080</tt>)")
-        addDescriptiveFormInput("Google Calendar*", inpGcalName, "Trage hier den Kalendernamen ein um die Google Integration einzuschalten.")
-        addDescriptiveFormInput("GMail Addresse", inpGmailAddress, "Trage hier deine GMail Adresse ein für das Versenden von E-Mails.")
-        addDescriptiveFormInput("Google API ID", inpGapiClientId, "Um die Google API nutzen zu können, brauchst du eine Zugangs-ID.<br/>" +
-                "Credentials sind erstellbar in der Google API Console.<br/>" +
-                "Bsp.: <tt>123456789012-aaaabbbbccccddddeeeefffffaaaabb.apps.googleusercontent.com</tt>")
-        addDescriptiveFormInput("Google API Secret", inpGapiClientSecret, "Das zugehörige Passwort.<br/>" +
-                "Bsp.: <tt>AABBCCDDDaabbccdd12345678</tt>")
-        addDescriptiveFormInput("Mail Subject", inpConfirmMailSubject, "Bestätigungsmail Vorlage welche die selben Variablen nutzen kann wie der Mail Body.")
-        // for available variables see: AppointmentConfirmationerImpl
-        addDescriptiveFormInput("Mail Body", inpConfirmMailBody.scrolled(), "Bestätigungsmail Vorlage. Mögliche Variablen: \${name}, \${dateStart?datetime}, \${dateEnd?datetime}.")
-        addLastColumnsFilled()
-    }
-
 }
 
 class PreferencesSwingWindow @Inject constructor(
