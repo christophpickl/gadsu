337e518412b9a5a6b84e1cc5117da9a143a8da07
Christoph Pickl <c.pickl@meo-energy.com>
2017-04-05 18:55:48 +0200

    moving stuff to kpot

--- src/main/kotlin/at/cpickl/gadsu/service/lang.kt
+++ src/main/kotlin/at/cpickl/gadsu/service/lang.kt
@@ -7,31 +7,30 @@ import java.io.Closeable
 import java.io.File
 import java.util.HashMap
 
-
+@Deprecated("Outsourced to kpotpourri.", ReplaceWith("use different import statement"))
 fun <K, V> Map<K, V>.verifyNoIntersection(that: Map<K, V>) {
 //    if (this.keys.any { that.containsKey(it) }) throw GadsuException("Expected no intersections! This: $this. That: $that.")
     this.keys.forEach { if (that.containsKey(it)) throw GadsuException("Expected no intersections! This: $this. That: $that.") }
 }
 
-@Deprecated("Outsourced to kpotpourri.", ReplaceWith("this.nullIfEmpty()"))
+@Deprecated("Outsourced to kpotpourri.", ReplaceWith("use different import statement"))
 fun String.nullIfEmpty() = if (isEmpty()) null else this
 
-
+@Deprecated("Outsourced to kpotpourri.", ReplaceWith("use different import statement"))
 fun String.wrapIf(condition: Boolean, wrappingLeft: String, wrappingRight: String) = if (condition) wrappingLeft + this + wrappingRight else this
 
+@Deprecated("Outsourced to kpotpourri.", ReplaceWith("use different import statement"))
 fun String.wrapParenthesisIf(condition: Boolean) = wrapIf(condition, "(", ")")
 
+@Deprecated("Outsourced to kpotpourri.", ReplaceWith("use different import statement"))
 fun String.htmlize() = "<html>" + this.replace("\n", "<br/>") + "</html>"
 
+@Deprecated("Outsourced to kpotpourri.", ReplaceWith("use different import statement"))
 fun String.saveToFile(target: File) {
     Files.write(this, target, Charsets.UTF_8)
 }
 
-//fun String.times(count: Int): String = StringBuilder().let { sb ->
-//    repeat(count) {
-//        sb.append(this)
-//    }
-//}.toString()
+@Deprecated("Outsourced to kpotpourri.", ReplaceWith("use different import statement"))
 fun String.times(count: Int): String {
     val symbol = this
     return StringBuilder().apply {
@@ -39,16 +38,10 @@ fun String.times(count: Int): String {
     }.toString()
 }
 
+@Deprecated("Outsourced to kpotpourri.", ReplaceWith("use different import statement"))
 fun String.removePreAndSuffix(search: String) = this.removePrefix(search).removeSuffix(search)
 
-fun <IN, OUT> IN?.nullOrWith(wither: (IN) -> OUT): OUT? {
-    if (this == null) {
-        return null
-    }
-    return wither(this)
-}
-
-
+@Deprecated("Outsourced to kpotpourri.", ReplaceWith("use different import statement"))
 fun <K, V> Iterable<Pair<K, V>>.toMutableMap(): HashMap<K, V> {
     val immutableMap = toMap()
     val map = HashMap<K, V>(immutableMap.size)
@@ -56,16 +49,18 @@ fun <K, V> Iterable<Pair<K, V>>.toMutableMap(): HashMap<K, V> {
     return map
 }
 
+@Deprecated("Outsourced to kpotpourri.", ReplaceWith("use different import statement"))
 fun Int.isBetweenInclusive(lower: Int, upper: Int) = this >= lower && this <= upper
 
+@Deprecated("Outsourced to kpotpourri.", ReplaceWith("use different import statement"))
 fun Int.forEach(code: () -> Unit) {
     for (i in 1..this) {
         code()
     }
 }
 
-
 val LOG_Closeable = LoggerFactory.getLogger(Closeable::class.java)!!
+@Deprecated("Outsourced to kpotpourri.", ReplaceWith("closeSilently()"))
 fun Closeable.closeQuietly() {
     try {
         close()
