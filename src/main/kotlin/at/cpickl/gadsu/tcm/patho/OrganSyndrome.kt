80fe9d8c98ba4530bea1044e8bfa2dfd3ae590f3
christophpickl <christoph.pickl@gmail.com>
2017-08-30 21:00:41 +0200

    #113 add more syndrom symptoms

--- src/main/kotlin/at/cpickl/gadsu/tcm/patho/OrganSyndrome.kt
+++ src/main/kotlin/at/cpickl/gadsu/tcm/patho/OrganSyndrome.kt
@@ -1,14 +1,14 @@
 package at.cpickl.gadsu.tcm.patho
 
 private val LuWindKaelteZungeUndPuls = setOf(
-        Symptom.ZungeNormal,
+        Symptom.NormaleZunge,
         Symptom.WeisserBelag,
         Symptom.VermehrterBelag,
         Symptom.DuennerBelag,
-        Symptom.PulsOberflaechlich,
-        Symptom.PulsGespannt,
-        Symptom.PulsVerlangsamt
-        )
+        Symptom.OberflaechlicherPuls,
+        Symptom.GespannterPuls,
+        Symptom.VerlangsamterPuls
+)
 
 enum class OrganSyndrome(
         val label: String,
@@ -21,6 +21,8 @@ enum class OrganSyndrome(
         val symptoms: Set<Symptom>
 ) {
 
+    // LUNGE
+    // =================================================================================================================
     LuQiMangel(
             label = "Lu-Qi-Mangel",
             description = "Hat etwas von einer Art Depression aber ohne der Trauer.",
@@ -30,7 +32,8 @@ enum class OrganSyndrome(
             symptoms = setOf(
                     Symptom.Kurzatmigkeit,
                     Symptom.Husten,
-                    Symptom.VermehrtDuennesSputum,
+                    Symptom.DuennerSchleim,
+                    Symptom.VermehrterSchleim,
                     Symptom.Asthma,
                     Symptom.FlacheAtmung,
 
@@ -44,14 +47,14 @@ enum class OrganSyndrome(
                     Symptom.Erkaeltungen,
                     Symptom.AversionKaelte,
 
-                    Symptom.ZungeNormal,
-                    Symptom.ZungeBlass,
-                    Symptom.ZungeGeschwollen,
+                    Symptom.NormaleZunge,
+                    Symptom.BlasseZunge,
+                    Symptom.GeschwolleneZunge,
 
-                    Symptom.PulsSchwach,
-                    Symptom.PulsWeich,
-                    Symptom.PulsLeer
-                    )),
+                    Symptom.SchwacherPuls,
+                    Symptom.WeicherPuls,
+                    Symptom.LeererPuls
+            )),
     LuYinMangel(
             label = "Lu-Yin-Mangel",
             organ = ZangOrgan.Lung,
@@ -64,18 +67,18 @@ enum class OrganSyndrome(
                     Symptom.TrockenerHusten,
                     Symptom.HitzeGefuehlAbends,
 
-                    Symptom.ZungeRot,
-                    Symptom.ZungeTrocken,
+                    Symptom.RoteZunge,
+                    Symptom.TrockeneZunge,
                     Symptom.WenigBelag,
 
-                    Symptom.SchleimFehlend,
-                    Symptom.SchleimWenig,
-                    Symptom.SchleimKlebrig,
-                    Symptom.SchleimBissiBlut,
+                    Symptom.KeinSchleim,
+                    Symptom.WenigSchleim,
+                    Symptom.KlebrigerSchleim,
+                    Symptom.BlutInSchleim,
 
-                    Symptom.PulsBeschleunigt,
-                    Symptom.PulsDuenn,
-                    Symptom.PulsSchwach
+                    Symptom.BeschleunigterPuls,
+                    Symptom.DuennerPuls,
+                    Symptom.SchwacherPuls
             ))),
     LuWindKaelteWind(
             label = "Wind Wind Kälte attackiert Lu",
@@ -87,7 +90,7 @@ enum class OrganSyndrome(
                     Symptom.FroestelnMitEtwasFieber,
                     Symptom.Schwitzen,
                     Symptom.AversionWind
-                    ).plus(LuWindKaelteZungeUndPuls)
+            ).plus(LuWindKaelteZungeUndPuls)
     ),
     LuWindKaelteKaelte(
             label = "Kälte Wind Kälte attackiert Lu",
@@ -103,10 +106,10 @@ enum class OrganSyndrome(
                     Symptom.Kopfschmerzen,
                     Symptom.Husten,
                     Symptom.Schnupfen,
-                    Symptom.KlarerSputum,
-                    Symptom.WaessrigerSputum,
+                    Symptom.KlarerSchleim,
+                    Symptom.WaessrigerSchleim,
                     Symptom.VerstopfteNase
-                    ).plus(LuWindKaelteZungeUndPuls)
+            ).plus(LuWindKaelteZungeUndPuls)
     ),
     LuWindHitze(
             label = "Wind Hitze attackiert Lu",
@@ -117,7 +120,7 @@ enum class OrganSyndrome(
                     Symptom.FieberStaerkerAlsFroesteln,
                     Symptom.Schwitzen,
                     Symptom.Husten,
-                    Symptom.SchleimGelb,
+                    Symptom.GelberSchleim,
                     Symptom.VerstopfteNase,
                     Symptom.Schnupfen,
                     Symptom.Halsschmerzen,
@@ -126,13 +129,13 @@ enum class OrganSyndrome(
                     Symptom.MehrDurst,
                     Symptom.MoechteKaltesTrinken,
 
-                    Symptom.ZungeRoteSpitze,
+                    Symptom.RoteZungenspitze,
                     Symptom.DuennerBelag,
                     Symptom.GelberBelag,
 
-                    Symptom.PulsOberflaechlich,
-                    Symptom.PulsBeschleunigt
-                    )
+                    Symptom.OberflaechlicherPuls,
+                    Symptom.BeschleunigterPuls
+            )
     ),
     LuTrocken(
             label = "Trockenheit attackiert Lu",
@@ -146,31 +149,205 @@ enum class OrganSyndrome(
                     Symptom.Nasenbluten,
                     Symptom.Heiserkeit,
                     Symptom.TrockenerHusten,
-                    Symptom.SchleimWenig,
-                    Symptom.SchleimFehlend,
+                    Symptom.WenigSchleim,
+                    Symptom.KeinSchleim,
                     Symptom.ZaeherSchleim,
-                    Symptom.SchleimBissiBlut,
+                    Symptom.BlutInSchleim,
                     Symptom.LeichteKopfschmerzen,
 
-                    Symptom.ZungeRoteSpitze,
+                    Symptom.RoteZungenspitze,
                     Symptom.GelberBelag,
                     Symptom.DuennerBelag,
                     Symptom.TrockenerBelag,
 
-                    Symptom.PulsOberflaechlich,
-                    Symptom.PulsBeschleunigt
+                    Symptom.OberflaechlicherPuls,
+                    Symptom.BeschleunigterPuls
             )
     ),
-    LuSchleim(
-            label = "Schleimretention in Lu",
+    // @LuSchleim generell:
+    //     - MP produziert schleim, Lu lagert ihn
+    //     - ursachen: MP-Qi/Yang-Mangel, Lu-Qi-Mangel
+    LuSchleimKalt(
+            label = "Kalte Schleimretention in Lu",
             organ = ZangOrgan.Lung,
             tendency = MangelUeberfluss.Ueberfluss,
+            symptoms = `Allgemeine Lunge Schleim Symptome`.plus(setOf(
+                    Symptom.WeisserSchleim,
+                    Symptom.TrueberSchleim,
+                    Symptom.ReichlichSchleim,
+                    Symptom.Blaesse,
+                    Symptom.Muedigkeit,
+                    Symptom.KaelteGefuehl,
+                    Symptom.WeisserBelag,
+                    Symptom.LangsamerPuls
+            ))
+    ),
+    LuSchleimHeiss(
+            label = "Heisse Schleimretention in Lu",
+            organ = ZangOrgan.Lung,
+            tendency = MangelUeberfluss.Ueberfluss,
+            symptoms = `Allgemeine Lunge Schleim Symptome`.plus(setOf(
+                    Symptom.GelberSchleim,
+                    Symptom.ReichlichSchleim,
+                    Symptom.Fieber,
+                    Symptom.HitzeZeichen,
+                    Symptom.GelberBelag,
+                    Symptom.BraunerBelag,
+                    Symptom.BeschleunigterPuls
+            ))
+    ),
+
+    // HERZ
+    // =================================================================================================================
+
+    HeQiXu(
+            label = "He-Qi-Mangel",
+            organ = ZangOrgan.Heart,
+            tendency = MangelUeberfluss.Mangel,
             symptoms = setOf(
-                    Symptom.Heiserkeit // TODO
+                    Symptom.LeuchtendeBlaesse,
+                    Symptom.Muedigkeit,
+                    Symptom.Schwaeche,
+                    Symptom.Palpitationen,
+                    Symptom.Kurzatmigkeit,
+                    Symptom.StarkesSchwitzen, // va am tag
+                    Symptom.MentaleMuedigkeit,
+
+                    Symptom.SchwacherPuls,
+                    Symptom.LeererPuls,
+                    Symptom.UnregelmaessigerPuls,
+
+                    Symptom.BlasseZunge,
+                    Symptom.GeschwolleneZunge,
+                    Symptom.LaengsrissInZunge
+            )
+    ),
+    HeYangXu(
+            label = "He-Yang-Mangel",
+            organ = ZangOrgan.Heart,
+            tendency = MangelUeberfluss.Mangel,
+            symptoms = HeQiXu.symptoms.plus(setOf(
+                    Symptom.BlassesGesicht,
+                    Symptom.WeissesGesicht,
+                    Symptom.ThorakalesEngegefuehl,
+                    Symptom.KaelteGefuehl,
+                    Symptom.KalteHaende,
+                    Symptom.KalteFuesse,
+
+                    Symptom.SchwacherPuls,
+                    Symptom.LangsamerPuls,
+                    Symptom.UnregelmaessigerPuls,
+
+                    Symptom.BlasseZunge,
+                    Symptom.FeuchteZunge,
+                    Symptom.GeschwolleneZunge
+            ))
+    ),
+    // He Yang Erschoepfung (ausgepraegter he yang xu) => hat noch 3 unterarten...
+    HeBlutXu(
+            label = "He-Blut-Mangel",
+            organ = ZangOrgan.Heart,
+            tendency = MangelUeberfluss.Mangel,
+            symptoms = setOf( // plus(allgemeiner blut mangel symptoms)
+                    Symptom.StumpfeBlaesse,
+                    Symptom.Schwindel,
+                    Symptom.Aengstlichkeit,
+                    Symptom.Schreckhaftigkeit,
+                    Symptom.Unruhe,
+                    Symptom.Schlafstoerungen,
+                    Symptom.VieleTraeume,
+                    Symptom.Konzentrationsstoerungen,
+                    Symptom.SchlechteMerkfaehigkeit,
+                    Symptom.Palpitationen,
+
+                    Symptom.BlasseZunge,
+                    Symptom.DuennerPuls
+            )
+    ),
+    HeYinXu(
+            label = "He-Yin-Mangel",
+            organ = ZangOrgan.Heart,
+            tendency = MangelUeberfluss.Mangel,
+            symptoms = setOf( // plus(allgemeiner yin mangel = palpitationen, unruhe, gereiztheit)
+                    Symptom.FuenfZentrenHitze,
+                    Symptom.Nachtschweiss,
+                    Symptom.HitzeGefuehlAbends,
+                    Symptom.RoteWangenflecken,
+                    Symptom.TrockenerMund,
+                    Symptom.MehrDurstSpaeter,
+                    Symptom.Unruhe,
+                    Symptom.Gereiztheit,
+                    Symptom.Schlafstoerungen,
+                    Symptom.VieleTraeume,
+                    Symptom.RoteZunge,
+                    Symptom.RoteZungenspitze,
+                    Symptom.DuennerBelag,
+                    Symptom.FehlenderBelag,
+                    Symptom.BeschleunigterPuls,
+                    Symptom.DuennerPuls
+            )
+    ),
+    HeFeuer(
+            label = "Loderndes Herz Feuer",
+            organ = ZangOrgan.Heart,
+            tendency = MangelUeberfluss.Ueberfluss,
+            symptoms = setOf(
+                    Symptom.Blaesse
+            )
+    ),
+    HeSchleimBeunruhigt(
+            label = "Schleim-Feuer beunruhigt das Herz",
+            organ = ZangOrgan.Heart,
+            tendency = MangelUeberfluss.Ueberfluss,
+            symptoms = setOf(
+                    Symptom.Blaesse
+            )
+    ),
+    HeSchleimVerstopft(
+            label = "Schleim verstopft die Herzöffnungen",
+            organ = ZangOrgan.Heart,
+            tendency = MangelUeberfluss.Ueberfluss,
+            symptoms = setOf(
+                    Symptom.Blaesse
+            )
+    ),
+    HeBlutStau(
+            label = "Herz Blut Stauung",
+            organ = ZangOrgan.Heart,
+            tendency = MangelUeberfluss.Ueberfluss,
+            symptoms = setOf(
+                    Symptom.Blaesse
             )
     )
-    //
-    //
-    //
+
+    // NIERE
+    // =================================================================================================================
+
+
+    // LEBER
+    // =================================================================================================================
+
+
+    // MILZ
+    // =================================================================================================================
+
+
 
 }
+
+private val `Allgemeine Lunge Schleim Symptome` = setOf(
+        Symptom.ThorakalesEngegefuehl,
+        Symptom.VoelleGefuehl,
+        Symptom.Atemnot,
+        Symptom.Asthma,
+        Symptom.Husten,
+        Symptom.ReichlichSchleim,
+        Symptom.RasselndeKehle,
+        Symptom.WenigAppetit,
+        Symptom.Breichreiz,
+        Symptom.DickerBelag,
+        Symptom.FeuchterBelag,
+        Symptom.SchmierigerBelag,
+        Symptom.SchluepfrigerPuls,
+        Symptom.OberflaechlicherPuls
+)
--- src/main/kotlin/at/cpickl/gadsu/tcm/patho/Symptom.kt
+++ src/main/kotlin/at/cpickl/gadsu/tcm/patho/Symptom.kt
@@ -16,6 +16,7 @@ enum class SymptomCategory {
     Farbe,
     Gesicht,
     Hoeren,
+    Herz,
     Mens,
     Misc,
     Psycho,
@@ -84,9 +85,11 @@ sealed class Symptom(
 
     // -----------------------------------------------------------------------------------------------------------------
 
-    // MENS
+    // MISC
     // =================================================================================================================
-    abstract class MensSymptom(source: SymptomSource) : Symptom(SymptomCategory.Mens, source)
+    abstract class MiscSymptom(source: SymptomSource) : Symptom(SymptomCategory.Misc, source)
+    object ThorakalesEngegefuehl : MiscSymptom(NOT_IMPLEMENTED)
+    object Schwindel : MiscSymptom(NOT_IMPLEMENTED)
 
     // ATMUNG
     // =================================================================================================================
@@ -94,71 +97,97 @@ sealed class Symptom(
     object Asthma : AtmungSymptom(NOT_IMPLEMENTED)
     object FlacheAtmung : AtmungSymptom(NOT_IMPLEMENTED)
     object Kurzatmigkeit : AtmungSymptom(NOT_IMPLEMENTED)
+    object Atemnot : AtmungSymptom(NOT_IMPLEMENTED)
+    // trockenheit
+    object Heiserkeit : AtmungSymptom(NOT_IMPLEMENTED)
     object TrockenerHals : AtmungSymptom(NOT_IMPLEMENTED)
     object TrockenerRachen : AtmungSymptom(NOT_IMPLEMENTED)
     object TrockeneNase : AtmungSymptom(NOT_IMPLEMENTED)
     object TrockenerMund : AtmungSymptom(NOT_IMPLEMENTED)
     object TrockenerHusten : AtmungSymptom(NOT_IMPLEMENTED)
-    object Husten : AtmungSymptom(NOT_IMPLEMENTED)
-    object Stimmbaender : AtmungSymptom(NOT_IMPLEMENTED)
-    object Heiserkeit : AtmungSymptom(NOT_IMPLEMENTED)
-    object KratzenderHals : AtmungSymptom(NOT_IMPLEMENTED)
-    object Halsschmerzen : AtmungSymptom(NOT_IMPLEMENTED)
-    object RoterHals : AtmungSymptom(NOT_IMPLEMENTED)
-    object RoterRachen : AtmungSymptom(NOT_IMPLEMENTED)
+    // nase
     object Schnupfen : AtmungSymptom(NOT_IMPLEMENTED)
     object VerstopfteNase : AtmungSymptom(NOT_IMPLEMENTED)
     object Nasenbluten : AtmungSymptom(NOT_IMPLEMENTED)
+    // misc
+    object Husten : AtmungSymptom(NOT_IMPLEMENTED)
+    object Stimmbaender : AtmungSymptom(NOT_IMPLEMENTED)
+    object KratzenderHals : AtmungSymptom(NOT_IMPLEMENTED)
+    object RasselndeKehle : AtmungSymptom(NOT_IMPLEMENTED)
+    object Halsschmerzen : AtmungSymptom(NOT_IMPLEMENTED)
+    object RoterHals : AtmungSymptom(NOT_IMPLEMENTED)
+    object RoterRachen : AtmungSymptom(NOT_IMPLEMENTED)
 
     // EMOTION
     // =================================================================================================================
     abstract class EmotionSymptom(source: SymptomSource) : Symptom(SymptomCategory.Emotion, source)
     object TrauererloseDepression : EmotionSymptom(NOT_IMPLEMENTED)
+    object Aengstlichkeit : EmotionSymptom(NOT_IMPLEMENTED)
+    object Schreckhaftigkeit : EmotionSymptom(NOT_IMPLEMENTED)
+    object Unruhe : EmotionSymptom(NOT_IMPLEMENTED)
+    object Gereiztheit : EmotionSymptom(NOT_IMPLEMENTED)
 
     // ENERGIE
     // =================================================================================================================
     abstract class EnergieSymptom(source: SymptomSource) : Symptom(SymptomCategory.Energie, source)
     object EnergieMangel : EnergieSymptom(NOT_IMPLEMENTED)
     object Muedigkeit : EnergieSymptom(NOT_IMPLEMENTED)
+    object MentaleMuedigkeit : EnergieSymptom(NOT_IMPLEMENTED)
+    object Schwaeche : EnergieSymptom(NOT_IMPLEMENTED)
 
     // ESSEN
     // =================================================================================================================
     abstract class EssenSymptom(source: SymptomSource) : Symptom(SymptomCategory.Essen, source)
+    object WenigAppetit : EssenSymptom(NOT_IMPLEMENTED)
 
     // FARBE
     // =================================================================================================================
     abstract class FarbeSymptom(source: SymptomSource) : Symptom(SymptomCategory.Farbe, source)
-    object Blaesse : FarbeSymptom(NOT_IMPLEMENTED)
+    object Blaesse : FarbeSymptom(NOT_IMPLEMENTED) // hat mehrere untertypen
+    object StumpfeBlaesse : FarbeSymptom(NOT_IMPLEMENTED)
+    object LeuchtendeBlaesse : FarbeSymptom(NOT_IMPLEMENTED)
 
     // GESICHT
     // =================================================================================================================
     abstract class GesichtSymptom(source: SymptomSource) : Symptom(SymptomCategory.Gesicht, source)
+    object WeissesGesicht : GesichtSymptom(NOT_IMPLEMENTED)
+    object BlassesGesicht : GesichtSymptom(NOT_IMPLEMENTED)
+
+    // HERZ
+    // =================================================================================================================
+    abstract class HerzSymptom(source: SymptomSource) : Symptom(SymptomCategory.Herz, source)
+    object Palpitationen : HerzSymptom(NOT_IMPLEMENTED)
 
     // HOEREN
     // =================================================================================================================
     abstract class HoerenSymptom(source: SymptomSource) : Symptom(SymptomCategory.Hoeren, source)
 
-    // MISC
+    // MENS
     // =================================================================================================================
-    abstract class MiscSymptom(source: SymptomSource) : Symptom(SymptomCategory.Misc, source)
+    abstract class MensSymptom(source: SymptomSource) : Symptom(SymptomCategory.Mens, source)
 
     // PSYCHO
     // =================================================================================================================
     abstract class PsychoSymptom(source: SymptomSource) : Symptom(SymptomCategory.Psycho, source)
+    object SchlechteMerkfaehigkeit : PsychoSymptom(NOT_IMPLEMENTED)
+    object Konzentrationsstoerungen : PsychoSymptom(NOT_IMPLEMENTED)
 
     // PULS
     // =================================================================================================================
     abstract class PulsSymptom(source: SymptomSource) : Symptom(SymptomCategory.Puls, source) {
         constructor(pulse: PulseProperty) : this(SymptomSource.PulseSource(pulse))
     }
-    object PulsLeer : PulsSymptom(NOT_IMPLEMENTED)
-    object PulsWeich : PulsSymptom(PulseProperty.Soft)
-    object PulsSchwach : PulsSymptom(NOT_IMPLEMENTED)
-    object PulsDuenn : PulsSymptom(NOT_IMPLEMENTED)
-    object PulsOberflaechlich : PulsSymptom(NOT_IMPLEMENTED)
-    object PulsGespannt : PulsSymptom(NOT_IMPLEMENTED)
-    object PulsVerlangsamt : PulsSymptom(NOT_IMPLEMENTED)
-    object PulsBeschleunigt : PulsSymptom(NOT_IMPLEMENTED)
+    object LeererPuls : PulsSymptom(NOT_IMPLEMENTED)
+    object WeicherPuls : PulsSymptom(PulseProperty.Soft)
+    object SchwacherPuls : PulsSymptom(NOT_IMPLEMENTED)
+    object DuennerPuls : PulsSymptom(NOT_IMPLEMENTED)
+    object SchluepfrigerPuls : PulsSymptom(NOT_IMPLEMENTED)
+    object OberflaechlicherPuls : PulsSymptom(NOT_IMPLEMENTED)
+    object GespannterPuls : PulsSymptom(NOT_IMPLEMENTED)
+    object VerlangsamterPuls : PulsSymptom(NOT_IMPLEMENTED)
+    object LangsamerPuls : PulsSymptom(NOT_IMPLEMENTED)
+    object BeschleunigterPuls : PulsSymptom(NOT_IMPLEMENTED)
+    object UnregelmaessigerPuls : PulsSymptom(NOT_IMPLEMENTED) // leitsymptom He
 
     // SCHMERZEN
     // =================================================================================================================
@@ -171,27 +200,39 @@ sealed class Symptom(
     // =================================================================================================================
     abstract class SchlafSymptom(source: SymptomSource) : Symptom(SymptomCategory.Schlaf, source)
     object Traeumen : SchlafSymptom(NOT_IMPLEMENTED)
+    object VieleTraeume : SchlafSymptom(NOT_IMPLEMENTED)
+    object Schlafstoerungen : SchlafSymptom(NOT_IMPLEMENTED) // hat untertypen
     object EinschlafStoerungen : SchlafSymptom(NOT_IMPLEMENTED)
 
     // SCHLEIM
     // =================================================================================================================
     abstract class SchleimSymptom(source: SymptomSource) : Symptom(SymptomCategory.Schleim, source)
-    object VermehrtDuennesSputum : SchleimSymptom(NOT_IMPLEMENTED)
-    object SchleimFehlend : SchleimSymptom(NOT_IMPLEMENTED)
-    object SchleimWenig : SchleimSymptom(NOT_IMPLEMENTED)
-    object SchleimKlebrig : SchleimSymptom(NOT_IMPLEMENTED)
-    object SchleimBissiBlut : SchleimSymptom(NOT_IMPLEMENTED)
-    object SchleimGelb : SchleimSymptom(NOT_IMPLEMENTED)
-    object KlarerSputum : SchleimSymptom(NOT_IMPLEMENTED)
-    object WaessrigerSputum : SchleimSymptom(NOT_IMPLEMENTED)
+    // farbe
+    object WeisserSchleim : SchleimSymptom(NOT_IMPLEMENTED)
+    object GelberSchleim : SchleimSymptom(NOT_IMPLEMENTED)
+    object KlarerSchleim : SchleimSymptom(NOT_IMPLEMENTED)
+    object TrueberSchleim : SchleimSymptom(NOT_IMPLEMENTED)
+    // menge
+    object VermehrterSchleim : SchleimSymptom(NOT_IMPLEMENTED)
+    object ReichlichSchleim : SchleimSymptom(NOT_IMPLEMENTED) // synonym?!
+    object WenigSchleim : SchleimSymptom(NOT_IMPLEMENTED)
+    object KeinSchleim : SchleimSymptom(NOT_IMPLEMENTED)
+    // konsistenz
+    object WaessrigerSchleim : SchleimSymptom(NOT_IMPLEMENTED)
+    object KlebrigerSchleim : SchleimSymptom(NOT_IMPLEMENTED)
     object ZaeherSchleim : SchleimSymptom(NOT_IMPLEMENTED)
+    object DuennerSchleim : SchleimSymptom(NOT_IMPLEMENTED)
+    // misc
+    object BlutInSchleim : SchleimSymptom(NOT_IMPLEMENTED)
 
     // SCHWEISS
     // =================================================================================================================
     abstract class SchweissSymptom(source: SymptomSource) : Symptom(SymptomCategory.Schweiss, source)
-    object LeichtesSchwitzen : SchlafSymptom(SymptomSource.XPropSource(XProps.Temperature, XProps.TemperatureOpts.SweatEasily))
+    object StarkesSchwitzen : SchlafSymptom(SymptomSource.XPropSource(XProps.Temperature, XProps.TemperatureOpts.SweatEasily))
     object Schwitzen : SchlafSymptom(SymptomSource.XPropSource(XProps.Temperature, XProps.TemperatureOpts.SweatEasily))
+    object LeichtesSchwitzen : SchlafSymptom(SymptomSource.XPropSource(XProps.Temperature, XProps.TemperatureOpts.SweatEasily))
     object KeinSchwitzen : SchlafSymptom(SymptomSource.XPropSource(XProps.Temperature, XProps.TemperatureOpts.SweatEasily))
+    object Nachtschweiss : SchlafSymptom(SymptomSource.XPropSource(XProps.Temperature, XProps.TemperatureOpts.SweatEasily))
 
     // SEHEN
     // =================================================================================================================
@@ -216,6 +257,14 @@ sealed class Symptom(
     object FroestelnMitEtwasFieber : TemperaturSymptom(NOT_IMPLEMENTED)
     object FroestelnStarkerAlsFieber : TemperaturSymptom(NOT_IMPLEMENTED)
     object FieberStaerkerAlsFroesteln : TemperaturSymptom(NOT_IMPLEMENTED)
+    object KaelteGefuehl : TemperaturSymptom(NOT_IMPLEMENTED)
+    object Fieber : TemperaturSymptom(NOT_IMPLEMENTED)
+    // TODO #115 dieses sollte dann auf viele andere symptoms matchen
+    object HitzeZeichen : TemperaturSymptom(NOT_IMPLEMENTED)
+    object KalteHaende : TemperaturSymptom(NOT_IMPLEMENTED)
+    object KalteFuesse : TemperaturSymptom(NOT_IMPLEMENTED)
+    object FuenfZentrenHitze : TemperaturSymptom(NOT_IMPLEMENTED)
+    object RoteWangenflecken : TemperaturSymptom(NOT_IMPLEMENTED)
 
     // TRINKEN
     // =================================================================================================================
@@ -223,6 +272,7 @@ sealed class Symptom(
     object WenigDurst : TrinkenSymptom(NOT_IMPLEMENTED)
     object KeinDurst : TrinkenSymptom(NOT_IMPLEMENTED)
     object MehrDurst : TrinkenSymptom(NOT_IMPLEMENTED)
+    object MehrDurstSpaeter : TrinkenSymptom(NOT_IMPLEMENTED) // untertyp von MehrDurst
     object MoechteKaltesTrinken : TrinkenSymptom(NOT_IMPLEMENTED)
 
     // URIN
@@ -232,24 +282,35 @@ sealed class Symptom(
     // VERDAUUNG
     // =================================================================================================================
     abstract class VerdauungSymptom(source: SymptomSource) : Symptom(SymptomCategory.Verdauung, source)
+    object Breichreiz : VerdauungSymptom(NOT_IMPLEMENTED)
+    object VoelleGefuehl : MiscSymptom(NOT_IMPLEMENTED)
 
     // ZUNGE
     // =================================================================================================================
     abstract class ZungeSymptom(source: SymptomSource) : Symptom(SymptomCategory.Zunge, source)
-    object ZungeNormal : ZungeSymptom(NOT_IMPLEMENTED)
-    object ZungeRoteSpitze : ZungeSymptom(NOT_IMPLEMENTED)
-    object ZungeGeschwollen : ZungeSymptom(NOT_IMPLEMENTED)
-    object ZungeTrocken : ZungeSymptom(NOT_IMPLEMENTED)
+    object NormaleZunge : ZungeSymptom(NOT_IMPLEMENTED)
+    object GeschwolleneZunge : ZungeSymptom(NOT_IMPLEMENTED)
+    object TrockeneZunge : ZungeSymptom(NOT_IMPLEMENTED)
+    object FeuchteZunge : ZungeSymptom(NOT_IMPLEMENTED)
+    object LaengsrissInZunge : ZungeSymptom(NOT_IMPLEMENTED)
     // farbe
-    object ZungeBlass : ZungeSymptom(NOT_IMPLEMENTED)
-    object ZungeRot : ZungeSymptom(NOT_IMPLEMENTED)
-    // belag
-    object WenigBelag : ZungeSymptom(NOT_IMPLEMENTED)
-    object VermehrterBelag : ZungeSymptom(NOT_IMPLEMENTED)
+    object BlasseZunge : ZungeSymptom(NOT_IMPLEMENTED)
+    object RoteZunge : ZungeSymptom(NOT_IMPLEMENTED)
+    object RoteZungenspitze : ZungeSymptom(NOT_IMPLEMENTED)
+    // belag - farbe
     object WeisserBelag : ZungeSymptom(NOT_IMPLEMENTED)
     object GelberBelag : ZungeSymptom(NOT_IMPLEMENTED)
+    object BraunerBelag : ZungeSymptom(NOT_IMPLEMENTED)
+    // belag - menge
+    object FehlenderBelag : ZungeSymptom(NOT_IMPLEMENTED)
+    object WenigBelag : ZungeSymptom(NOT_IMPLEMENTED)
+    object VermehrterBelag : ZungeSymptom(NOT_IMPLEMENTED) // synonym?!
+    object DickerBelag : ZungeSymptom(NOT_IMPLEMENTED)
+    // belag - misc
     object DuennerBelag : ZungeSymptom(NOT_IMPLEMENTED)
     object TrockenerBelag : ZungeSymptom(NOT_IMPLEMENTED)
+    object FeuchterBelag : ZungeSymptom(NOT_IMPLEMENTED)
+    object SchmierigerBelag : ZungeSymptom(NOT_IMPLEMENTED)
 
 }
 
