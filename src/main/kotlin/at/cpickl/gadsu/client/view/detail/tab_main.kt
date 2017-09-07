8d309cf2c07eacc3abfc82633a157ae76737ddcd
christophpickl <christoph.pickl@gmail.com>
2017-09-07 18:58:07 +0200

    #119 reorder: ext first

--- src/main/kotlin/at/cpickl/gadsu/client/view/detail/tab_main.kt
+++ src/main/kotlin/at/cpickl/gadsu/client/view/detail/tab_main.kt
@@ -49,8 +49,8 @@ class ClientTabMain(
     // base
     val inpFirstName = fields.newTextField("Vorname", { it.firstName }, ViewNames.Client.InputFirstName)
     val inpLastName = fields.newTextField("Nachname", { it.lastName }, ViewNames.Client.InputLastName)
-    val inpNickNameInt = fields.newTextField("Spitzname (int.)", { it.nickNameInt }, ViewNames.Client.InputNickNameInt)
     val inpNickNameExt = fields.newTextField("Spitzname (ext.)", { it.nickNameExt }, ViewNames.Client.InputNickNameExt)
+    val inpNickNameInt = fields.newTextField("Spitzname (int.)", { it.nickNameInt }, ViewNames.Client.InputNickNameInt)
     val inpGender = fields.newComboBox(Gender.Enum.orderedValues, initialClient.gender, "Geschlecht", { it.gender }, ViewNames.Client.InputGender)
     val inpBirthday = fields.newDatePicker(initialClient.birthday, "Geburtstag", { it.birthday }, ViewNames.Client.InputBirthdayPrefix)
     val outAge = JLabel().bold()
