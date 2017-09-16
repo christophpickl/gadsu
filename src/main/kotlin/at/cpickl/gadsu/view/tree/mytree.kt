7450ffb0cc2209facb94ea9e566f087ad3894116
christophpickl <christoph.pickl@gmail.com>
2017-09-16 21:07:34 +0200

    old tree doesnt work, new tree2 does (so far)

--- src/main/kotlin/at/cpickl/gadsu/view/tree/mytree.kt
+++ src/main/kotlin/at/cpickl/gadsu/view/tree/mytree.kt
@@ -1,44 +1,68 @@
 package at.cpickl.gadsu.view.tree
 
+import com.github.christophpickl.kpotpourri.common.enforceAllBranchesCovered
+import com.github.christophpickl.kpotpourri.common.logging.LOG
+import com.google.common.base.MoreObjects
 import java.awt.Component
 import javax.swing.JScrollPane
 import javax.swing.JTree
+import javax.swing.event.TreeExpansionEvent
+import javax.swing.event.TreeExpansionListener
 import javax.swing.event.TreeSelectionEvent
+import javax.swing.event.TreeWillExpandListener
 import javax.swing.tree.DefaultMutableTreeNode
+import javax.swing.tree.DefaultTreeCellRenderer
 import javax.swing.tree.DefaultTreeModel
+import javax.swing.tree.DefaultTreeSelectionModel
 import javax.swing.tree.MutableTreeNode
+import javax.swing.tree.TreePath
+import javax.swing.tree.TreeSelectionModel
 
+data class MyTreeNode<G, L>(
+        val myNode: MyNode<G, L>,
+        val label: String
+) : DefaultMutableTreeNode(label) {
+    init {
+        // back reference
+        myNode.treeNode = this
+    }
+}
 
 sealed class MyNode<G, L> {
     abstract val label: String
     abstract val subNodes: List<MyNode<G, L>>
-
     abstract fun toTreeNode(): MutableTreeNode
 
-    data class MyGroupNode<G, L>(
+    lateinit var treeNode: MyTreeNode<G, L>
+
+    override fun toString() = MoreObjects.toStringHelper(this)
+            .add("label", label)
+            .toString()
+
+    class MyGroupNode<G, L>(
             val entity: G,
             override val label: String,
-            override val subNodes: List<MyNode<G, L>>
+            override val subNodes: List<MyLeafNode<G, L>>
     ) : MyNode<G, L>() {
-        override fun toTreeNode() = DefaultMutableTreeNode(label).apply {
+        override fun toTreeNode() = MyTreeNode(this, label).apply {
             subNodes.forEach { subNode ->
                 add(subNode.toTreeNode())
             }
         }
     }
 
-    data class MyLeafNode<G, L>(
+    class MyLeafNode<G, L>(
             val entity: L,
             override val label: String
     ) : MyNode<G, L>() {
-        override fun toTreeNode() = DefaultMutableTreeNode(label)
+        override fun toTreeNode() = MyTreeNode(this, label)
 
         override val subNodes: List<MyLeafNode<G, L>> = emptyList()
     }
 
 }
 
-class MyTreeModel<G, L>(private val nodes: List<MyNode<G, L>>) {
+class MyTreeModel<G, L>(val nodes: List<MyNode<G, L>>) {
     val toSwingModel by lazy {
         DefaultTreeModel(DefaultMutableTreeNode("root ignored").apply {
             nodes.forEach { node ->
@@ -49,57 +73,186 @@ class MyTreeModel<G, L>(private val nodes: List<MyNode<G, L>>) {
 
 }
 
+private class TreeNodeRenderer<G, L> : DefaultTreeCellRenderer() {
+    override fun getTreeCellRendererComponent(tree: JTree, value: Any, sel: Boolean, expanded: Boolean, leaf: Boolean, row: Int, hasFocus: Boolean): Component {
+        super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus)
+        val node = (value as MyTreeNode<G, L>)
+        text = node.label
+        return this
+    }
+}
+
 // https://docs.oracle.com/javase/tutorial/uiswing/components/tree.html#create
 class MyTree<G, L>(private val model: MyTreeModel<G, L>) {
 
-    private val tree = JTree()
-
-    init {
-        updateModel(model)
-
-        tree.showsRootHandles = true
-        tree.isRootVisible = false
-/*
- tree.setCellRenderer(new DefaultTreeCellRenderer() {
-      public Component getTreeCellRendererComponent(JTree tree,
-          Object value, boolean sel, boolean expanded, boolean leaf,
-          int row, boolean hasFocus) {
-        super.getTreeCellRendererComponent(tree, value, sel, expanded,
-            leaf, row, hasFocus);
-        if (!((InvisibleNode) value).isVisible()) {
-          setForeground(Color.yellow);
-        }
-        return this;
-      }
-    });
- */
-
-        // TODO selection just as for MyList
-        // tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
-
-        tree.addTreeSelectionListener { event: TreeSelectionEvent ->
-            val node = tree.getLastSelectedPathComponent() as DefaultMutableTreeNode?
-            if (node == null) {
-                // nothing selected
+    private val tree = object : JTree() {
+        override fun fireValueChanged(e: TreeSelectionEvent) {
+            // suppress selection of group node when it is collapsed
+            if (isValid) {
+                super.fireValueChanged(e)
             } else {
-                println("selected: $node")
-//                if (node.isLeaf) {
-//                    val leafNode = node.userObject as MyNode.MyLeafNode<G, L>
-//                    println(leafNode)
-//                } else {
-//                    val groupNode = node.userObject as MyNode.MyGroupNode<G, L>
-//                    println(groupNode)
-//                }
+                super.clearSelection()
             }
         }
     }
 
+    private val log = LOG {}
+
+    private fun TreePath.toMyTreeNode() = lastPathComponent as MyTreeNode<G, L>
+
+    private val transientSelections = mutableMapOf<TreePath, List<TreePath>>()
+
     fun asComponent(): Component = JScrollPane(tree)
 
-    private fun updateModel(model: MyTreeModel<G, L>) {
-        val root = DefaultMutableTreeNode("root")
-        root.add(DefaultMutableTreeNode("sub1 aaa"))
+    private fun printSelected() {
+        model.nodes.forEach { node ->
+            val treeNode = node.treeNode
+            val groupNode = treeNode.myNode as MyNode.MyGroupNode<G, L>
+
+            groupNode.subNodes.forEach { subNode ->
+                val pathToSubNode = TreePath(subNode.treeNode.path)
+                val isSelected = tree.isPathSelected(pathToSubNode)
+                println("${node.label} => ${subNode.label} => selected: $isSelected")
+            }
+        }
+    }
+
+    private fun storeTransientSelections() {
+        model.nodes.forEach { node ->
+            val treeNode = node.treeNode
+            val treePath = TreePath(treeNode.path)
+            val groupNode = treeNode.myNode as MyNode.MyGroupNode<G, L>
+
+            // store selection transiently
+            transientSelections[treePath] = groupNode.subNodes.filter { subNode ->
+                val pathToSubNode = TreePath(subNode.treeNode.path)
+                val isSelected = tree.isPathSelected(pathToSubNode)
+//                println("${node.label} => ${subNode.label} => selected: $isSelected")
+                isSelected
+            }.map { TreePath(it.treeNode.path) }
+        }
+    }
+
+    private fun restoreTransientSelections() {
+        transientSelections.forEach { _, selectedPaths ->
+            selectedPaths.forEach { selectedPath ->
+                tree.addSelectionPath(selectedPath)
+            }
+        }
+    }
+
+    init {
         tree.model = model.toSwingModel
+
+        tree.showsRootHandles = true
+        tree.isRootVisible = false
+        tree.cellRenderer = TreeNodeRenderer<G, L>()
+
+        tree.addTreeWillExpandListener(object : TreeWillExpandListener {
+            override fun treeWillCollapse(event: TreeExpansionEvent) {
+                log.trace { "treeWillCollapse(event)" }
+                storeTransientSelections()
+
+//                val treeNode = event.path.toMyTreeNode()
+//                val groupNode = treeNode.myNode as MyNode.MyGroupNode<G, L>
+//
+//                // store selection transiently
+//                transientSelections[event.path] = groupNode.subNodes.filter { subNode ->
+//                    val pathToSubNode = TreePath(subNode.treeNode.path)
+//                    val isSelected = tree.isPathSelected(pathToSubNode)
+//                    isSelected
+//                }.map { TreePath(it.treeNode.path) }
+            }
+
+            override fun treeWillExpand(event: TreeExpansionEvent) {
+                log.trace { "treeWillExpand(event)" }
+                val selections = transientSelections[event.path] ?: return
+                selections.forEach { selectionPath ->
+                    tree.addSelectionPath(selectionPath)
+                }
+            }
+        })
+
+        tree.addTreeExpansionListener(object : TreeExpansionListener {
+            override fun treeExpanded(event: TreeExpansionEvent) {
+                log.trace { "treeExpanded(event)" }
+            }
+
+            override fun treeCollapsed(event: TreeExpansionEvent) {
+                log.trace { "treeCollapsed(event)" }
+            }
+        })
+
+        tree.selectionModel = object : DefaultTreeSelectionModel() {
+            init {
+                selectionMode = TreeSelectionModel.DISCONTIGUOUS_TREE_SELECTION
+            }
+
+            override fun addSelectionPath(path: TreePath) {
+
+                val treeNode = path.toMyTreeNode()
+
+                when (treeNode.myNode) {
+                    is MyNode.MyGroupNode -> {
+                        // TODO toggleOpenClosedGroup(path) ... otherwise have to double click the entry
+                        printSelected()
+                        restoreTransientSelections()
+                        printSelected()
+                        log.trace { "addSelectionPath(path=$path) ... suppressed for group nodes." }
+                        return
+                    }
+                    is MyNode.MyLeafNode -> {
+                        log.trace { "addSelectionPath(path=$path)" }
+                        super.addSelectionPath(path)
+                    }
+                }.enforceAllBranchesCovered
+            }
+
+            private fun toggleOpenClosedGroup(path: TreePath) {
+                log.trace { "toggleOpenClosedGroup(path=$path) tree.isExpanded(path)=${tree.isExpanded(path)}" }
+                val treeNode = path.toMyTreeNode()
+                when (treeNode.myNode) {
+                    is MyNode.MyLeafNode<G, L> -> return
+                    is MyNode.MyGroupNode<G, L> -> if (tree.isExpanded(path)) tree.collapsePath(path) else tree.expandPath(path)
+                }.enforceAllBranchesCovered
+            }
+
+            override fun setSelectionPath(path: TreePath) {
+                log.trace { "setSelectionPath(path=$path)" }
+
+                if (isPathSelected(path)) {
+                    removeSelectionPath(path)
+                } else {
+                    addSelectionPath(path)
+                }
+//                treeNode.myNode.entity for Group/Leaf
+            }
+
+            override fun setSelectionPaths(path: Array<out TreePath>) {
+                log.trace { "setSelectionPaths(path=${path.contentToString()})" }
+                super.setSelectionPaths(path)
+            }
+
+            override fun removeSelectionPaths(paths: Array<out TreePath>) {
+                log.trace { "removeSelectionPaths(path=${paths.contentToString()})" }
+                super.removeSelectionPaths(paths)
+            }
+
+            override fun removeSelectionPath(path: TreePath) {
+                log.trace { "removeSelectionPath(path=$path)" }
+                super.removeSelectionPath(path)
+            }
+
+        }
+
+        tree.addTreeSelectionListener { event: TreeSelectionEvent ->
+            val node = tree.getLastSelectedPathComponent() as DefaultMutableTreeNode?
+            log.trace {"tree selected: $node"}
+            if (node == null) {
+                // nothing selected
+            } else {
+            }
+        }
     }
 
 }
--- src/test/kotlin/non_test/_main_/view/tree_search.kt
+++ src/test/kotlin/non_test/_main_/view/tree_search.kt
@@ -7,13 +7,69 @@ import at.cpickl.gadsu.view.tree.LiveSearchField
 import at.cpickl.gadsu.view.tree.MyNode
 import at.cpickl.gadsu.view.tree.MyTree
 import at.cpickl.gadsu.view.tree.MyTreeModel
+import at.cpickl.gadsu.view.tree.MyTreeNode
+import com.github.christophpickl.kpotpourri.common.enforceAllBranchesCovered
+import com.github.christophpickl.kpotpourri.common.logging.LOG
 import non_test.Framed
 import java.awt.BorderLayout
 import javax.swing.JPanel
+import javax.swing.JScrollPane
+import javax.swing.JTree
+import javax.swing.tree.TreePath
+import javax.swing.tree.TreeSelectionModel
 
 // TODOs
 // - group nodes NOT selectable
 // - select changes selection like list
+// enableHoverListener
+
+private class MyTreeModel<G, L>(private val oldModel: TreeSelectionModel) : TreeSelectionModel by oldModel {
+    private val log = LOG {}
+
+    // toggle selection on click
+    override fun setSelectionPath(path: TreePath) {
+        log.trace { "setSelectionPath(path=$path)" }
+
+        if (isPathSelected(path)) {
+            removeSelectionPath(path)
+        } else {
+            addSelectionPath(path)
+        }
+    }
+
+    override fun addSelectionPath(path: TreePath) {
+        val treeNode = path.toMyTreeNode()
+        when (treeNode.myNode) {
+            is MyNode.MyGroupNode -> {
+                // TODO toggleOpenClosedGroup(path) ... otherwise have to double click the entry
+                log.trace { "addSelectionPath(path=$path) ... suppressed for group nodes." }
+                return
+            }
+            is MyNode.MyLeafNode -> {
+                log.trace { "addSelectionPath(path=$path)" }
+                oldModel.addSelectionPath(path)
+            }
+        }.enforceAllBranchesCovered
+    }
+
+    private fun TreePath.toMyTreeNode() = lastPathComponent as MyTreeNode<G, L>
+}
+
+
+private class MyTree2<G, L>(private val model: MyTreeModel<G, L>) : JTree(model.toSwingModel) {
+//private class MyTree2<G, L>(root: DefaultMutableTreeNode) : JTree(root) {
+    private val log = LOG {}
+
+    init {
+        showsRootHandles = true
+        isRootVisible = false
+
+        val selectionModelOld = selectionModel
+        selectionModel = non_test._main_.view.MyTreeModel<G, L>(selectionModelOld)
+    }
+
+}
+
 fun main(args: Array<String>) {
     val searchField = LiveSearchField()
     val tree = MyTree(listOf(XProps.ChiStatus, XProps.BodyConception).toTreeModel())
@@ -22,7 +78,9 @@ fun main(args: Array<String>) {
         JPanel().apply {
             layout = BorderLayout()
             add(searchField.asComponent(), BorderLayout.NORTH)
-            add(tree.asComponent(), BorderLayout.CENTER)
+//            add(tree.asComponent(), BorderLayout.CENTER)
+            add(JScrollPane(MyTree2(listOf(XProps.ChiStatus, XProps.BodyConception).toTreeModel())))
+
         }
     }
 }
